fc878e6b258765edc5e307cc2bce4ed16b906dfb
Stephen A. Fuqua <stephen@safnet.com>
2021-12-31 20:21:10 -0600

    As fully restored as it will get

--- serve.py
+++ serve.py
@@ -1,6 +1,7 @@
 import os
 from http.server import HTTPServer, CGIHTTPRequestHandler
 
+print('http://localhost:876')
 os.chdir('.')
 server_object = HTTPServer(server_address=('', 876), RequestHandlerClass=CGIHTTPRequestHandler)
 server_object.serve_forever()
