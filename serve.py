b61274491dc062fe1903beb59cfabd21751daa6d
Stephen Fuqua <stephen@safnet.com>
2026-04-25 15:31:47 -0500

    Serve www directory

--- serve.py
+++ serve.py
@@ -2,6 +2,6 @@ import os
 from http.server import HTTPServer, CGIHTTPRequestHandler
 
 print('http://localhost:876')
-os.chdir('.')
+os.chdir('./www')
 server_object = HTTPServer(server_address=('', 876), RequestHandlerClass=CGIHTTPRequestHandler)
 server_object.serve_forever()
